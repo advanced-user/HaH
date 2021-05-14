0f87e67ce6521e54bfdb39bf02cb2b6e8eb86356
Proger2k <stolbetskiy.yura@gmail.com>
2021-05-14 08:24:56 +0300

    Xpath

---
+++ Lab4/js.js
@@ -0,0 +1,7 @@
+document.evaluate("//brands[1]/brand[1]/name[1]/text()", document, null, XPathResult.ANY_TYPE, null).iterateNext();
+document.evaluate("//brands[1]/brand[2]/description[1]/text()", document, null, XPathResult.ANY_TYPE, null).iterateNext();
+
+for (let unit of $x('//@unit'))
+    console.log(unit.nodeValue);
+
+$x("//brand[./name/text()='Xiaomi Redmi 9' and ./characteristics/weight[@unit='g']]")
